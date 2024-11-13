64673a6e39baa61cce9c5fc4b1adabdb5e6152d4
chrizweb <ibanexgiox7@gmail.com>
2024-11-13 02:35:24 -0600

    Creaccion de middlewares y excepciones

--- Data/Usuarios/UsuarioRepository.cs
+++ Data/Usuarios/UsuarioRepository.cs
@@ -1,7 +1,9 @@
 
 
+using System.Net;
 using Microsoft.AspNetCore.Identity;
 using NetKubernetes.Dtos.UsuarioDtos;
+using NetKubernetes.Middleware;
 using NetKubernetes.Models;
 using NetKubernetes.Token;
 
@@ -27,7 +29,7 @@ namespace NetKubernetes.Data.Usuarios {
       _contexto = contexto;
       _usuarioSesion = usuarioSesion;
     }
-
+    /*********************************************************************/
     private UsuarioResponseDto TransformerUserToUserDto(Usuario usuario){
       return new UsuarioResponseDto{
         Id = usuario.Id,
@@ -39,35 +41,83 @@ namespace NetKubernetes.Data.Usuarios {
         Token = _jwtGenerador.CrearToken(usuario)
       };
     }
-
+    /*********************************************************************/
     public async Task<UsuarioResponseDto> GetUsuario() {
       
       var usuario = await _userManager.FindByNameAsync(_usuarioSesion.ObtenerUsuarioSesion()); 
 
+      if(usuario is null){
+        throw new MiddlewareException(
+          HttpStatusCode.Unauthorized, 
+          new {mensaje = "Token Invalido!!!"}
+        ); 
+      }
+
       return TransformerUserToUserDto(usuario!);
     }
-
-    public async Task<UsuarioResponseDto> Login(UsuarioLoginRequestDto request) {
+    /*********************************************************************/
+    public async Task<UsuarioResponseDto> Login(UsuarioLoginRequestDto loginDto) {
       
-      var usuario = await _userManager.FindByEmailAsync(request.Email!);
-      await _signInManager.CheckPasswordSignInAsync(usuario!, request.Password!, false);
+      var usuario = await _userManager.FindByEmailAsync(loginDto.Email!);
 
-      return TransformerUserToUserDto(usuario!);
+      if(usuario is null){
+        throw new MiddlewareException(HttpStatusCode.Unauthorized, 
+        new {mensaje = "Email ó Password Incorrectos!!!"}
+        ); 
+      }
+
+      var resultado = await _signInManager.CheckPasswordSignInAsync(usuario!, loginDto.Password!, false);
+
+      if(resultado.Succeeded){
+      return TransformerUserToUserDto(usuario);
+      }
+      throw new MiddlewareException(
+        HttpStatusCode.Unauthorized,
+        new {mensaje = "Email ó Password Incorrectos!!!"}
+      );
+
     }
+    /*********************************************************************/
+    public async Task<UsuarioResponseDto> RegistroUsuario(UsuarioRegistroRequestDto usuarioRequest) {
 
-    public async Task<UsuarioResponseDto> RegistroUsuario(UsuarioRegistroRequestDto request) {
+      var existeEmail = await _contexto.Users
+      .Where(u => u.Email == usuarioRequest.Email).AnyAsync();
+
+      if(existeEmail){
+        throw new MiddlewareException(
+        HttpStatusCode.BadRequest,
+        new {mensaje = "El email ya existe!!!"}
+      );
+      }
+
+       var existeUserName = await _contexto.Users
+      .Where(u => u.UserName == usuarioRequest.UserName).AnyAsync();
+
+      if(existeUserName){
+        throw new MiddlewareException(
+        HttpStatusCode.BadRequest,
+        new {mensaje = "El username ya existe!!!"}
+      );
+      }
      
       var usuario = new Usuario{
-        Nombre = request.Nombre,
-        Apellido = request.Apellido,
-        Telefono = request.Telefono,
-        Email = request.Email,
-        UserName = request.UserName,
+        Nombre = usuarioRequest.Nombre,
+        Apellido = usuarioRequest.Apellido,
+        Telefono = usuarioRequest.Telefono,
+        Email = usuarioRequest.Email,
+        UserName = usuarioRequest.UserName,
       };
 
-      await _userManager.CreateAsync(usuario!, request.Password!);
+      var resultado = await _userManager.CreateAsync(usuario!, usuarioRequest.Password!);
 
+      if(resultado.Succeeded){
       return TransformerUserToUserDto(usuario);
+
+      }
+
+      throw new Exception("Error al registrar usurio!!!");
+
     }
+    /*********************************************************************/
   }
 }
---
+++ Middleware/ManagerMiddleware.cs
@@ -0,0 +1,54 @@
+
+
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NetKubernetes.Middleware {
+  public class ManagerMiddleware {
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ManagerMiddleware> _logger;
+
+    public ManagerMiddleware(RequestDelegate next, ILogger<ManagerMiddleware> logger) {
+      _next = next;
+      _logger = logger;
+    }
+  /*********************************************************************/
+    public async Task Invoke(HttpContext context) {
+      try {
+
+        await _next(context);
+      }
+      catch (Exception ex) {
+
+        await ManagerExceptionAsync(context, ex, _logger);
+      }
+    }
+  /*********************************************************************/
+  private async Task ManagerExceptionAsync(HttpContext context, Exception ex, ILogger<ManagerMiddleware> logger){
+    object? errores = null;
+    
+    switch(ex){
+      case MiddlewareException me:
+        logger.LogError(ex, "Middleware Error");
+        errores = me.Errores;
+        context.Response.StatusCode = (int)me.Codigo;
+        break;
+
+      case Exception e:
+        logger.LogError(ex, "Error de Servidor");
+        errores = string.IsNullOrWhiteSpace(e.Message) ? "Error" : e.Message;
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        break;
+
+    }
+
+    context.Response.ContentType = "application/json";
+    var resultado = string.Empty;
+    if(errores != null){
+      resultado = JsonConvert.SerializeObject(new {errores});
+    }
+    await context.Response.WriteAsync(resultado);
+  }
+
+  }
+}
